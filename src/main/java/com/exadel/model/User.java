acf5b2e72ba03f18b4b4f19990439f19aa3eae49
Inf1mum <Elfvadim@mail.ru>
2014-07-15 14:46:50 +0300

    Vadim
    commit by Vadim

---
+++ src/main/java/com/exadel/model/User.java
@@ -0,0 +1,83 @@
+package com.exadel.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+/**
+ * Created by Вадим on 15.07.2014.
+ */
+@Entity
+@Table(name="user",schema = "", catalog = "ms")
+public class User implements Serializable {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "skype")
+    private String skype;
+
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSkype() {
+        return skype;
+    }
+    public void setSkype(String skype) {
+        this.skype = skype;
+    }
+
+
+
+}
