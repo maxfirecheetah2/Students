4bbfa007a001ab4caccecb6e559ff7617fee471a
maxfirecheetah2 <maxfirecheetah2@gmail.com>
2014-07-18 17:32:13 +0400

    by Max

---
+++ src/main/java/com/exadel/entity/Skill.java
@@ -0,0 +1,11 @@
+package com.exadel.entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: X
+ * Date: 18.07.14
+ * Time: 16:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class Skill {
+}
