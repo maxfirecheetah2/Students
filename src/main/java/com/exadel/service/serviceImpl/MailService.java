5d150f16ab80b25675aa111fbf4c2e628ad0e78e
Inf1mum <Elfvadim@mail.ru>
2014-08-11 10:54:10 +0300

    commit by Vadim

--- src/main/java/com/exadel/service/serviceImpl/MailService.java
+++ src/main/java/com/exadel/service/serviceImpl/MailService.java
@@ -1,28 +1,37 @@
 package com.exadel.service.serviceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class MailService {
+public class MailService{
 
         @Autowired
+        @Qualifier("mailSender")
         private MailSender mailSender;
 
 
-    public void sendMail(String to, String subject, String body)
-    {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setFrom("exadell@gmail.com");
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+
+    public void sendMail(final String to, final String subject, final String body){
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                SimpleMailMessage message = new SimpleMailMessage();
+                message.setTo(to);
+                message.setFrom("exadell@gmail.com");
+                message.setSubject(subject);
+                message.setText(body);
+                 mailSender.send(message);
+
+            }
+        });
+        th.start();
+
     }
-
-
 
 }
